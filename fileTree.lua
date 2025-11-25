ca2a97858aee6d24c56fd1bfa3471c11f6fbe948
pik <piotrkacz12@wp.pl>
2025-11-26 00:03:37 +0100

    added addition of files and folders

--- fileTree.lua
+++ fileTree.lua
@@ -1,4 +1,4 @@
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 local micro = import("micro")
 local command = import("micro/command")
@@ -12,39 +12,18 @@ local treeBuffer
 local viewBuffer
 
 local openKey = "o"
+local newFileKey = "n"
+local newFolderKey = "f"
 
 local treeTable = {}
 local outView = ""
 
 -- 1. Initialize the plugin
 function init()
-	-- Create a command ':mytree'
 	-- ========== WORKING ==================
-	config.MakeCommand("mytree", openTree, config.NoComplete)
 	config.MakeCommand("filetree", openTree, config.NoComplete)
 end
 
--- 2. functions that catches eventes
--- catches special keys
-function preAction(bp, action)
-	return true
-end
-
--- catches standard keys
-function preRune(bp, action)
-	if bp.Buf.Settings["filetype"] ~= "filetree" then
-		return true
-	end
-
-	if action == openKey then
-		-- micro.InfoBar("Opening")
-		selectItem(bp)
-		return false -- this blocks natural reaction
-	end
-
-	return true
-end
--- 3.
 -- Function on command mytree or filetree
 function openTree(bp)
 	viewBuffer = bp
@@ -53,64 +32,62 @@ function openTree(bp)
 
 	rebuildView()
 
-	-- Open a Vertical Split with this buffer
 	micro.CurPane():VSplitIndex(treeBuffer, true)
 
-	-- Resize it (make it small, like a sidebar)
 	micro.CurPane():ResizePane(180)
-
-	-- =========== TESTING ===============
-	-- bp.HandleCommand("open ".."file")
 end
 
+-- =============================================
+-- ================== REACTIONS =================
+-- =============================================
+
+function processAction(bp, action)
+	if action == openKey then
+		-- micro.InfoBar("Opening")
+		selectItem(bp)
+		return false -- this blocks natural reaction
+	end
+
+	if action == newFileKey then
+		createFile(bp)
+		return false
+	end
+
+	if action == newFolderKey then
+		createFolder(bp)
+		return false
+	end
+
+	return true
+end
+
+-- catches special keys
+function preAction(bp, action)
+	if bp.Buf.Settings["filetype"] ~= "filetree" then
+		return true
+	end
+	return processAction(bp, action)
+end
+
+-- catches standard keys
+function preRune(bp, action)
+	if bp.Buf.Settings["filetype"] ~= "filetree" then
+		return true
+	end
+	return processAction(bp, action)
+end
+
+-- ========================================
+-- ================= FETCHING =============
+-- ========================================
 function loadFolder(folderName)
 	shell.RunCommand("cd " .. folderName)
 end
 
 -- Loading content of specific directory
 function loadContentOfFolder(folderName)
-	-- shell.RunCommand("cd " .. folderName)
 	local output, err = shell.RunCommand("ls -a -F '" .. folderName .. "'/")
 	return output
-end
-
--- Wrapper for opening file:
-function openFile(bp, fileName)
-	local newBuf, err = buffer.NewBufferFromFile(fileName)
-
-	if err == nil then
-		bp:OpenBuffer(newBuf)
-	else
-		micro.InfoBar():Error(err)
-	end
-end
-
-function openFileInTab(bp, filePath)
-    -- bp:HandleCommand("tab "..filePath)
-    local cmd = string.format("tab %q", filePath)
-
-        -- Execute
-        bp:HandleCommand(cmd)
-end
-
-function fileNode(name, parentPath)
-	local isDir = false
-	local cleanName = name
-	if string.sub(name, -1) == "/" then
-		isDir = true
-		cleanName = string.sub(name, 1, -2)
-	end
-	local lastChar = string.sub(cleanName, -1)
-	    if lastChar == "*" or lastChar == "@" or lastChar == "|" or lastChar == "=" then
-	        cleanName = string.sub(cleanName, 1, -2)
-	    end
-	return {
-		name = cleanName,
-		path = filepath.Join(parentPath, cleanName),
-		isDir = isDir,
-		expanded = false,
-		children = {},
-	}
 end
 
 function fetchFiles(homeFolder)
@@ -127,11 +104,35 @@ function fetchFiles(homeFolder)
 	return tabTree
 end
 
+-- =======================================
+-- =============== FILE OPENING ==========
+-- =======================================
+
+-- Wrapper for opening file:
+function openFile(bp, fileName)
+	local newBuf, err = buffer.NewBufferFromFile(fileName)
+
+	if err == nil then
+		bp:OpenBuffer(newBuf)
+	else
+		micro.InfoBar():Error(err)
+	end
+end
+
+function openFileInTab(bp, filePath)
+	local cmd = string.format("tab %q", filePath)
+	bp:HandleCommand(cmd)
+end
+
+-- =========================================
+-- ============== PARSING DATA ==============
+-- =========================================
+
 function treeTableToString(treeTab, depth)
 	local tmp = ""
-    local indent = string.rep(" ",depth)
+	local indent = string.rep(" ", depth)
 	for k, file in ipairs(treeTab) do
-        tmp = tmp..indent
+		tmp = tmp .. indent
 		if file.name ~= "." and file.name ~= ".." then
 			if file.isDir then
 				if file.expanded then
@@ -151,25 +152,29 @@ function treeTableToString(treeTab, depth)
 	return tmp
 end
 
-function rebuildView()
-	outView = treeTableToString(treeTable, 0)
-	if treeBuffer ~= nil then
-		treeBuffer.Type.Readonly = false
-		treeBuffer:Replace(treeBuffer:Start(), treeBuffer:End(), outView)
-
-		treeBuffer.Type.Readonly = true
-		return
+function fileNode(name, parentPath)
+	local isDir = false
+	local cleanName = name
+	if string.sub(name, -1) == "/" then
+		isDir = true
+		cleanName = string.sub(name, 1, -2)
 	end
-
-	treeBuffer = buffer.NewBuffer(outView, "fileTree")
-	treeBuffer.Type.Scratch = true
-	treeBuffer.Type.Readonly = true
-	treeBuffer:SetOption("filetype", "filetree")
-	treeBuffer:SetOption("softwrap", "false")
-	treeBuffer:SetOption("ruler", "false")
-	treeBuffer:SetOption("statusformatl", "File Tree")
-	treeBuffer:SetOption("statusformatr", "")
+	local lastChar = string.sub(cleanName, -1)
+	if lastChar == "*" or lastChar == "@" or lastChar == "|" or lastChar == "=" then
+		cleanName = string.sub(cleanName, 1, -2)
+	end
+	return {
+		name = cleanName,
+		path = filepath.Join(parentPath, cleanName),
+		isDir = isDir,
+		expanded = false,
+		children = {},
+	}
 end
+
+-- ==============================
+-- =========== FINDING ==========
+-- ===============================
 
 function findItemByLine(nodes, lineId, currentLine)
 	for _, file in ipairs(nodes) do
@@ -193,6 +198,10 @@ function findItemByLine(nodes, lineId, currentLine)
 	return nil, currentLine
 end
 
+-- ===============================
+-- ============ ACTIONS ==========
+-- ===============================
+
 function selectItem(bp)
 	local cursorY = bp.Cursor.Loc.Y
 
@@ -214,5 +223,78 @@ function selectItem(bp)
 	bp.Cursor.Loc.Y = cursorY
 end
 
--- Possible TODOs:
--- maybe change to inserting instead of
+function createFolder(bp)
+	enterName(bp, "Folder", "Enter folder name")
+end
+
+function createFile(bp)
+	enterName(bp, "File", "Enter file name")
+end
+
+function enterName(bp, option, promptMessage)
+	-- option is to choose if file is created or folder
+	local cursorY = bp.Cursor.Loc.Y
+
+	local node, _ = findItemByLine(treeTable, cursorY + 1, 0)
+
+	if node == nil then
+		return
+	end
+
+	micro.InfoBar():Prompt(promptMessage, "", "file", nil, function(input)
+		if input == "" then
+			return
+		end
+		local path = node.path
+
+		if node.isDir == false then
+			path = filepath.Dir(path)
+		end
+
+		local fullPath = path .. "/" .. input
+
+		if option == "Folder" then
+			local err = os.Mkdir(fullPath, 0755)
+			if err ~= nil then
+				micro.InfoBar():Error("Error creating folder: " .. tostring(err))
+			else
+				micro.InfoBar():Message("Folder created: " .. input)
+			end
+		elseif option == "File" then
+			local file, err = io.open(fullPath, "w")
+			if file then
+				file:close()
+				micro.InfoBar():Message("File created: " .. input)
+			else
+				micro.InfoBar():Error("Error creating file: " .. tostring(err))
+			end
+		end
+		treeTable = fetchFiles(".")
+		rebuildView()
+		bp.Cursor.Loc.Y = cursorY
+	end)
+end
+
+-- ===========================
+-- =========== VIEW ==========
+-- ===========================
+
+function rebuildView()
+	outView = treeTableToString(treeTable, 0)
+	if treeBuffer ~= nil then
+		treeBuffer.Type.Readonly = false
+		treeBuffer:Replace(treeBuffer:Start(), treeBuffer:End(), outView)
+
+		treeBuffer.Type.Readonly = true
+		return
+	end
+
+	treeBuffer = buffer.NewBuffer(outView, "fileTree")
+	treeBuffer.Type.Scratch = true
+	treeBuffer.Type.Readonly = true
+	treeBuffer:SetOption("filetype", "filetree")
+	treeBuffer:SetOption("softwrap", "false")
+	treeBuffer:SetOption("ruler", "false")
+	treeBuffer:SetOption("statusformatl", "File Tree")
+	treeBuffer:SetOption("statusformatr", "")
+end
